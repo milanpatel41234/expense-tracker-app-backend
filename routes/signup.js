722575707d3b8532582592c03528c45dfa3899f7
milanpatel41234 <mpatel41234@gmail.com>
2023-09-19 17:33:22 +0530

    error handling in signup

--- routes/signup.js
+++ routes/signup.js
@@ -5,6 +5,12 @@ const signup = (req,res)=>{
     const newuser = req.body
 user.create(newuser)
 .then(result=>res.send(result))
-.catch(err =>res.status(500).send({message:'Error creating user',error: err}));
+.catch(err =>{
+    console.log(err.name)
+    if(err.name === 'SequelizeUniqueConstraintError'){
+        return res.send({message:'This email already exists',error: err})
+    }
+    res.status(500).send({message:'Some error occured, Please try after some time.',error: err})
+});
 }
 module.exports = signup;
